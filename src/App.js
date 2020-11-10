b04659882489cf0de127ef2dabe868d11304acc4
haeunsong <jamy0225@naver.com>
2020-11-11 01:59:57 +0900

    리액트 기초

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import React from "react"
+
+class App extends React.Component{
+  state={
+    isLoading:true,
+    movies:[]
+  };
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading:false});
+    },6000);
+  }
+  render(){
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading...":"We are ready"}</div>
+  }
+}
+
+
+export default App;
