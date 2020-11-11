8f80379a06937d8984524b3fdd7a25f4efb11e02
haeunsong <jamy0225@naver.com>
2020-11-11 22:22:49 +0900

    api 사용해서 영화 제목까지 불러오기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import { render } from "@testing-library/react";
 import React from "react"
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
   state={
     isLoading:true,
     movies:[]
   };
+
+  // async, await 중요!! 
+  getMovies = async()=> {
+    // axios가 끝날 때까지 기다려야한다는 것을 알려줌.
+  
+    const {
+      data:{
+        data:{movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies:movies,isLoading:false}); // 그냥 {movies}라고 써도 됨.
+  };
+
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading:false});
-    },6000);
+    this.getMovies();
   }
+
+
   render(){
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Loading...":"We are ready"}</div>
+    const {isLoading,movies} = this.state;
+    return <div>{isLoading ? "Loading...":movies.map(movie=>{
+      console.log(movie);
+      return <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.poster} 
+              />
+    })}</div>
   }
 }
 
